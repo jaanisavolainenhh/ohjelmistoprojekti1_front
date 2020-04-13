2d4e4ba054daaf11fdf76f9cbc491d466d7c874f
JoelVaarala <bgh275@myy.haaga-helia.fi>
2020-04-13 21:48:46 +0300

    vastaus + router

--- src/components/Vastaus.js
+++ src/components/Vastaus.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 export default function Vastaus() {
 
-    return (
+    const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        getResults();
+    },[])
+
+    const getResults = () => {
+        fetch('http://localhost:8080/api/vastauses') //https://salenpalikatback.herokuapp.com/api/vastauses
+        .then(response => response.json())
+        .then(jsonresult => {
+          //  console.log(jsonresult) // Tarkistin että toi harjoitusapi toimii
+            setResults(jsonresult._embedded.vastauses[1].vastaus); // jsonresult._embedded.vastauses.vastaus
+        })
+        .catch(err => console.error(err))
+    }
+
+    return ( // MUT MIKS TÄÄ EI TULOSTU
         <div>
-            
+           <p>{results}</p> 
         </div>
     )
+    
 }
