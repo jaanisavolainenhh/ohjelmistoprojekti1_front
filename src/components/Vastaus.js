fc2722e041cad33ebac30fe3c8912ff3cbee48a7
Jaani Savolainen <bgh273@myy.haaga-helia.fi>
2020-04-14 07:20:52 +0300

    Finaali?

--- src/components/Vastaus.js
+++ src/components/Vastaus.js
@@ -13,17 +13,37 @@ export default function Vastaus(props) {
         fetch( props.urlit + 'api/vastauses') //https://salenpalikatback.herokuapp.com/api/vastauses
         .then(response => response.json())
         .then(jsonresult => {
-
-            setResults(jsonresult._embedded.vastauses[0].vastaus); 
+            console.log("haetaaan vastaukset");
+            //setResults(jsonresult._embedded.vastauses[0].vastaus); 
             // Eli hakee tällä backendistä ensimmäisen kovakoodatun vastauksen, mutta halutaan taulun viimeisin vastaus
+            let lista = jsonresult._embedded.vastauses;
+            let lista2 = new Array();
+            lista.forEach(itemi => {
+                lista2.push(itemi.vastaus);
+            
+            })
+            setResults(lista2);
         })
         .catch(err => console.error(err))
     }
 
+
+    function PalautaVastaukset()
+    {
+        return(
+          
+            results.map((tulos,index) => {
+                return <div>{tulos}</div>
+            })
+
+        )
+    }
+
     return ( 
         <div>
-            <p>Kysymys: </p>
-           <p>{results}</p> 
+            <p>Annetut vastaukset: </p>
+  
+            <div> <PalautaVastaukset /></div>
         </div>
     )
     
