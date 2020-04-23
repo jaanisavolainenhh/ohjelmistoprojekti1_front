03425f592af8f44f241549bc2dee852d5d2c52e8
Jaani Savolainen <bgh273@myy.haaga-helia.fi>
2020-04-23 21:22:02 +0300

    Fixattu jaanin säädöt, kai. Takas brancheihin vain

--- src/components/Kysely.js
+++ src/components/Kysely.js
@@ -13,58 +13,17 @@ import Checkbox from '@material-ui/core/Checkbox';
 
 export default function Kysely(props) {
 
-<<<<<<< HEAD
-    const [kysymys, setKysymys] = React.useState([]);    // Käytetään kysymyksen esittämiseen.
-    // const [vaihtoehtoA, setVaihtoehtoA] = React.useState('');
-    // const [vaihtoehtoB, setVaihtoehtoB] = React.useState('');
-    // const [vaihtoehtoC, setVaihtoehtoC] = React.useState('');  // and the options if/when needed.
-    const [vaihtoehdot, setVaihtoehdot] = React.useState([]);
-    const [value, setValue] = React.useState([]); //radiobuttoni säätelee tämän arvoa ja lukee tästä valinnan.
-    const [vastaus, setVastaus] = React.useState({ vastaus: '', kysymys: { id: -1 } }); //Raakile versio vastaus oliosta, olennainen löytyy.
-
-    //Snackbariin statet
-    const [open, setOpen] = React.useState(false);
-    const [msg, setmsg] = React.useState('')
-    React.useEffect(() => {
-        fetch(props.urlit + 'api/kysymyses')
-            .then(result => result.json())
-            .then(jsonresult => {
-
-                setKysymys(jsonresult._embedded.kysymyses[0].kysymys);
-                //Tällä saadaan kysymyksen ID selville, vähän kömpelö mutta menköön alkuun
-                let saato = jsonresult._embedded.kysymyses[0]._links.self.href;
-                saato = parseInt(saato.replace(props.urlit + 'api/kysymyses/', ""));  //otetaan hreffistä pois alkuurli jotta jäljelle jää vain ID
-                setVastaus({ ...vastaus, kysymys: { id: saato } });
-                //console.log(props.urlit)
-            })
-            .catch(err => console.error(err))
-    }, [])
-=======
-  const [kysymys, setKysymys] = React.useState([]);    // Käytetään kysymyksen esittämiseen.
-  const [vaihtoehtoA, setVaihtoehtoA] = React.useState('');
-  const [vaihtoehtoB, setVaihtoehtoB] = React.useState('');
-  const [vaihtoehtoC, setVaihtoehtoC] = React.useState('');  // and the options if/when needed.
+  const [kysely, setKysely] = React.useState([{kysymykset: []}]);
+  //const [kysymys, setKysymys] = React.useState([]);    // Käytetään kysymyksen esittämiseen.
   const [vaihtoehdot, setVaihtoehdot] = React.useState([]);
   const [value, setValue] = React.useState([]); //radiobuttoni säätelee tämän arvoa ja lukee tästä valinnan.
   const [vastaus, setVastaus] = React.useState({ vastaus: '', kysymys: { id: -1 } }); //Raakile versio vastaus oliosta, olennainen löytyy.
->>>>>>> f3d70ad2dde47ae8a541c7caf0690b03838f1219
 
   //Snackbariin statet
   const [open, setOpen] = React.useState(false);
   const [msg, setmsg] = React.useState('')
   React.useEffect(() => {
-    fetch(props.urlit + 'api/kysymyses')
-      .then(result => result.json())
-      .then(jsonresult => {
-
-        setKysymys(jsonresult._embedded.kysymyses[0].kysymys);
-        //Tällä saadaan kysymyksen ID selville, vähän kömpelö mutta menköön alkuun
-        let saato = jsonresult._embedded.kysymyses[0]._links.self.href;
-        saato = parseInt(saato.replace(props.urlit + 'api/kysymyses/', ""));  //otetaan hreffistä pois alkuurli jotta jäljelle jää vain ID
-        setVastaus({ ...vastaus, kysymys: { id: saato } });
-        //console.log(props.urlit)
-      })
-      .catch(err => console.error(err))
+    JaaninUseEffecti();
   }, [])
 
   //skaalan merkit 1-5 + en osaa vastata
@@ -95,12 +54,6 @@ export default function Kysely(props) {
     },
   ];
 
-<<<<<<< HEAD
-            })
-            .catch(err => console.error(err))
-=======
-
-
 
   React.useEffect(() => {
 
@@ -115,11 +68,6 @@ export default function Kysely(props) {
         })
         setVaihtoehdot(lista2);
 
-        //console.log(vaihtoehdot)
-
-        setVaihtoehtoA(jsonresult._embedded.vaihtoehtoes[0].vaihtoehto);
-        setVaihtoehtoB(jsonresult._embedded.vaihtoehtoes[1].vaihtoehto);
-        setVaihtoehtoC(jsonresult._embedded.vaihtoehtoes[2].vaihtoehto);
       })
       .catch(err => console.error(err))
   }, [])
@@ -167,17 +115,6 @@ export default function Kysely(props) {
     setValue();
   }
 
-  function GeneroiVastaukset() { //Tätä ei nyt käytetä missään, kuiteskin mielenkiintonen toiminta
-
-    return LuoVaihtoehdot();
-    return (
-      vaihtoehdot.map((value, index => {
-        return <FormControlLabel value={index} control={<Radio />} label={index} key={index} />  //miks helvetissä index näyttää oikeasti vastausarvoa tässä ja ei anna vaihtaa  mapin valueta johonkin muhun? ## Note taitaa olla sen takia kun on  " index => {" eikä "index) => {" kuten Genervoivaihtoehdoissa()
-      }))
-    )
-  }
-
-
   function LuoVaihtoehdot() {
     return (
       vaihtoehdot.map((jotain, index) => {
@@ -193,7 +130,6 @@ export default function Kysely(props) {
 
   function SnackBarCompo() {
     return (
-
       <Snackbar
         open={open}
         autoHideDuration={3000}
@@ -206,7 +142,7 @@ export default function Kysely(props) {
   function TextFieldVastausJotain(props) {
     const [BBB, AAA] = React.useState('');
     return (
-      
+
       <div>
         <TextField id="outlined-basic" label="Nimi" variant="outlined" /> <br />
         <TextField id="outlined-basic" label="Sähköposti" variant="outlined" />< br />
@@ -216,189 +152,76 @@ export default function Kysely(props) {
   }
 
   function RadioGroupVastaus(props) {
-
     return (
-
-      <div> 
-
+      <div>
         <RadioGroup aria-label="kys" name="kys" value={value} onChange={handleChange}>
           <LuoVaihtoehdot />
-          {/* 
-                    <FormControlLabel value={vaihtoehtoA} control={<Radio />} label={vaihtoehtoA} />
-                    <FormControlLabel value={vaihtoehtoB} control={<Radio />} label={vaihtoehtoB} />
-                    <FormControlLabel value={vaihtoehtoC} control={<Radio />} label={vaihtoehtoC} /> */}
         </RadioGroup><br />
+      </div>
+    )
+  }
 
+  function JaaninUseEffecti() {
+    fetch('http://localhost:8080/kyselyt')
+      .then(response => response.json())
+      .then(res => {
+        setKysely(res)
+      })
+      .catch(err => console.log(err))
+  }
+
+
+  function KokoRoska() {
+    return (
+      <div>
+        Kyselyn otsikko vaikka
+        <PrinttaaKysymyksetJaVastaukset />
+                Kyselyn päättyminen
+      </div>
+    )
+
+  }
+
+  function PrinttaaKysymyksetJaVastaukset() {
+    return (
+      <div>
+        {
+          kysely.map((tulos, index) => {
+            console.log(tulos)
+            return (
+              <div>
+                < PrinttaaKysymys kysymykset={tulos.kysymykset} />
+              </div>
+            )
+          })
+        }
       </div>
 
-import React, { useState, useEffect, Divider } from 'react';
-import Kysymys from './Kysymys';
-export default function Kysely(props) {
+    )
 
-    const [kysely, setKysely] = useState([]);
+  }
 
-    useEffect(() => {
-        JaaninUseEffecti();
->>>>>>> f3d70ad2dde47ae8a541c7caf0690b03838f1219
-    }, [])
-
-
-    function JoelionUseEffecti() {
-        fetch('http://localhost:8080/api/kysymyses') // en saanut meidän apia auki (/kysely), joten hieman testi dataa generoidun kautta.
-            .then(response => response.json())
-            .then(res => {
-                console.log("haetaan kysely")
-                setKysely(res._embedded.kysymyses)
-            })
-            .catch(err => console.log(err))
-
-    }
-
-
-    function JaaninUseEffecti() {
-        fetch('http://localhost:8080/kyselyt')
-            .then(response => response.json())
-            .then(res => {
-                setKysely(res)
-            })
-            .catch(err => console.log(err))
-    }
-
-
-    function KokoRoska() {
-
+  function PrinttaaKysymys(props) {
+    return (
+      props.kysymykset.map((kysymys, index) => {
         return (
-            <div>
-                Kyselyn otsikko vaikka
-                <PrinttaaKysymyksetJaVastaukset />
-                Kyselyn päättyminen
+          <div>
+            {kysymys.kysymys}
+            <PrinttaaVaihtoehdot vaihtoehdot={kysymys.vaihtoehdot} kysymystyyppi={kysymys.tyyppi} />
+          </div>)
+      })
+    )
+  }
 
-            </div>
-        )
-
-    }
-
-    //setKysely(...kysely, )
-    //55 divi ja 56 sulku on esim turha potentiaalisesti, testin takia mukana
-    function PrinttaaKysymyksetJaVastaukset() {
-        return (
-            <div>
-                {
-                    kysely.map((tulos, index) => {
-                        console.log(tulos)
-                        return (
-                            <div> 
-                                {/* <Divider light /> */}
-                                < PrinttaaKysymys kysymykset={tulos.kysymykset} />
-                                {/* <Divider light /> */}
-                            </div>
-                        )
-                    })
-                }
-            </div>
-
-        )
-
-    }
-
-    function PrinttaaKysymys(props) {
-        return (
-            props.kysymykset.map((kysymys, index) => {
-                return (
-                    <div>
-                        {kysymys.kysymys}
-                        <PrinttaaVaihtoehdot vaihtoehdot={kysymys.vaihtoehdot} kysymystyyppi={kysymys.tyyppi} />
-                    </div>)
-            })
-        )
-    }
-
-    function PrinttaaVaihtoehdot(props) { //tyyppi propilla voidaan valita elinen componentti tähän switchin kautta.
-
-        return (
-            props.vaihtoehdot.map((vaihtoehto, index) => {
-                return (
-                    <div>
-                        {vaihtoehto.vaihtoehto}
-                    </div>)
-            })
-        )
-    }
-
-
-    //Jaanin Testailua
-    function Testi1(props) {
-        return (<div><Testi2 /> <Testi3 /> {props.viesti} </div>)
-    }
-    //Jaanin Testailua
-
-    function Testi2() {
-        return (<div> Testi 2 func</div>)
-    }
-    //Jaanin Testailua
-
-    function Testi3() {
-        return (<div> Testi 3 func</div>)
-    }
-
-    //Megahärpäke miten ei ihan kannata tehdä. Jos lisää divejä ja {} niin sitten tääkin lopulta toimis. Pilkottu paloihin
-    function LuoKysymykset() {
-        return (
-            kysely.map((item, index) => {
-                console.log(item);
-                return (
-                    item.kysymykset.map((kysymys, index2) => {
-                        console.log(kysymys.kysymys)
-                        return (
-                            kysymys.vaihtoehdot.map((vastaus, index3) => {
-                                return <div>{vastaus.vaihtoehto}</div>
-                                console.log(vastaus.vaihtoehto)
-                            })
-                        )
-                        return <div>{kysymys.kysymys}</div>
-                    }))
-            }
-            )
-        )
-        // return (<div> jotain </div>);
-    }
-    // jKysely mapin avulla hakee kysely statelta jokaisen kysymyksen käyttäen indexiä yksilöllistämiseen
-    // Selkeyden vuoksi tulostuu päällekkäisiin elementteihin (viimeinen tr+br kikkailua, jotta ei turhia varoituksia selaimessa)
-    // const jKysely = kysely.map((item, index) =>
-    //     <tbody>
-    //         <tr key={index}>
-    //             <td>{item.kysymys} (kys)</td>
-    //         </tr>
-    //         <tr>
-    //             <td>{item.tyyppi} (vaihtoehdot)</td>
-    //         </tr>
-    //         <tr>
-    //             <td> (VastausElementti) </td>
-    //         </tr>
-    //         <tr>
-    //             <td> <br /> </td>
-    //         </tr>
-    //     </tbody>
-    // )
+  function PrinttaaVaihtoehdot(props) { //tyyppi propilla voidaan valita elinen componentti tähän switchin kautta.
 
     return (
-
-<<<<<<< HEAD
-            <FormControl component="fieldset">
-                <h3>{kysymys}</h3>
-                <RadioGroup aria-label="kys" name="kys" value={value} onChange={handleChange}>
-                    <LuoVaihtoehdot />
-                </RadioGroup>
-            </FormControl>
-=======
-        // <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <div>
->>>>>>> f3d70ad2dde47ae8a541c7caf0690b03838f1219
-
-            {/* <Testi1 viesti="viestiteksti" /> */}
-            <KokoRoska />
-        </div>
-
+      props.vaihtoehdot.map((vaihtoehto, index) => {
+        return (
+          <div>
+            {vaihtoehto.vaihtoehto}
+          </div>)
+      })
     )
   }
 
@@ -417,10 +240,10 @@ export default function Kysely(props) {
     )
   }
 
-  function MonivalintaVastaus(props){
-    return(
+  function MonivalintaVastaus(props) {
+    return (
       <div>
-         <FormControl component="fieldset">
+        <FormControl component="fieldset">
           <FormGroup>
             <FormControlLabel
               control={<Checkbox checked={vastaus1} onChange={handlaaCheckboxei} name='vastaus1' />}
@@ -445,14 +268,22 @@ export default function Kysely(props) {
   // Returns "question" as fetch result, radio with 2 options and button to post value of the answer
   return (
     <div>
-        <FormControl component="fieldset">
-    < TextFieldVastausJotain />
-    < RadioGroupVastaus />
-    <SliderVastaus />
-      <MonivalintaVastaus />
-      <br /><br /><Button variant="contained" color="primary" onClick={() => postAnswer()}>Vastaa</Button>
-      < SnackBarCompo />
+      <FormControl component="fieldset">
+        <KokoRoska />
+
+
+
+        {/* < TextFieldVastausJotain />
+        < RadioGroupVastaus />
+        <SliderVastaus />
+        <MonivalintaVastaus /> */}
+        <br /><br /><Button variant="contained" color="primary" onClick={() => postAnswer()}>Vastaa</Button>
+        < SnackBarCompo />
       </FormControl>
     </div>
   )
+
+
+
 }
+
